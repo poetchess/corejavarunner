d5c3e718cdb2b8c9f0df9513a48f154364afa3b0
poetchess <ilvbaicai@126.com>
2017-02-08 15:31:10 +0800

    code and comment for section 6.1.1 and 6.1.2: interface concept and properties

---
+++ interfaces/interfaces/Employee.java
@@ -0,0 +1,42 @@
+package interfaces;
+
+//An interface is a set of requirements for the classes.
+//Interfaces never have instance fields.
+
+//Interface are not classes, cannot use 'new' operator to instantiate 
+//an interface. Comparable x; x = new Employee(...); is OK if Employee
+//implements Comparable.
+
+//Declaring the class to implement the given interface
+//Supplying definitions for all methods in the interface
+//Classes can implement multiple interfaces.
+public class Employee implements Comparable<Employee> {
+
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    //All methods in an interface are automatically public.
+    //All fields are always public static final.
+    //When implementing the interface, we must declare the method as public.
+    public int compareTo(Employee other) {
+        return Double.compare(salary, other.salary);
+    }
+}
