b865434a264ffe6837a48e279ce979b56e931576
poetchess <ilvbaicai@126.com>
2017-02-17 07:40:22 +0800

    comment for section 6.3.3/4/5: functional interfaces, method references

--- interfaces/lambda/LambdaTest.java
+++ interfaces/lambda/LambdaTest.java
@@ -19,6 +19,24 @@ public class LambdaTest {
         //If the parameter types of a lambda expression can be inferred, it 
         //can be omitted. The return type of a lambda expression is never 
         //specified, it is always inferred from context.
+
+        //Functional interface: An interface with a single abstract method.
+        //A lambda expression can be used whenever an object of a functional
+        //  interface is expected.
+        //Conversion to a functional interface is the only thing we can do with
+        //a lambda expression in Java.
+
+        //Method reference: Using a exsiting method that carries out the action
+        //and pass on to other code.
+        //Three cases:
+        //  1. object::instanceMethod
+        //     i.e. System.out::println <-> x -> System.out.println(x)
+        //  2. Class::staticMethod
+        //     i.e. Math::pow <-> (x, y) -> Math.pow(x, y)
+        //  3. Class::instanceMethod
+        //     i.e. String::compareToIgnoreCase <-> 
+        //              (x, y) -> x.compareToIgnoreCase(y)
+
         Arrays.sort(planets, (first, second) -> 
             first.length() - second.length());
 
