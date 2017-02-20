7f942e5c7f83a78e791a44bad2864051ff52da14
poetchess <ilvbaicai@126.com>
2017-02-20 16:02:43 +0800

    comment for section 6.4.2: special syntax rules for inner classes

--- interfaces/innerClass/InnerClassTest.java
+++ interfaces/innerClass/InnerClassTest.java
@@ -27,6 +27,14 @@ class TalkingClock {
     }
 
     public void start() {
+        //Inner object constructor can be written more explicitly as
+        //outerObject.new InnerClass(construction parameters)
+        //'new TimePrinter()' equals to 'this.new TimePrinter()'
+
+        //Since TimePrinter is a public inner class, we can write such
+        //code as:
+        //  TalkingClock jabberer = new TalkingClock(1000, true);
+        //  TalkingClock.TimePrinter listener = jabberer.new TimePrinter();
         ActionListener listener = new TimePrinter();
         Timer t = new Timer(interval, listener);
         t.start();
@@ -36,6 +44,8 @@ class TalkingClock {
     //methods can construct inner class objects.
     //Only inner class can be private. Regular class always has either package
     //or public visibility.
+    //Outside the scope of the outer class, inner class is referred as
+    //OuterClass.InnerClass
     public class TimePrinter implements ActionListener {
         public void actionPerformed(ActionEvent event) {
             System.out.println("At the tone, the time is " + new Date());
@@ -44,6 +54,9 @@ class TalkingClock {
             //An inner class object always gets an implicit reference to the
             //outer object. This reference is set in the inner class
             //constructor.
+
+            //OuterClass.this denotes the outer class reference.
+            //'beep' here is equal to 'TalkingClock.this.beep'
             if (beep) {
                 Toolkit.getDefaultToolkit().beep();
             }
