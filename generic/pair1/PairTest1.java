30714295af7c9c66e93d01c9932498502e53feed
poetchess <ilvbaicai@126.com>
2017-03-04 22:37:56 +0800

    code and comments for section 8.1/2: generic programming introduction

---
+++ generic/pair1/PairTest1.java
@@ -0,0 +1,33 @@
+package pair1;
+
+//Generic programming: writing code that can be reused for objects of many 
+//different types.
+//Type parameters: making the programs easier to read and safer.
+public class PairTest1 {
+    public static void main(String[] args) {
+        String[] words = {"Mary", "has", "a", "little", "lamb"};
+        Pair<String> mm = ArrayAlg.minmax(words);
+        System.out.println("min = " + mm.getFirst());
+        System.out.println("max = " + mm.getSecond());
+    }
+}
+
+class ArrayAlg {
+    public static Pair<String> minmax(String[] a) {
+        if (a == null || a.length == 0) {
+            return null;
+        }
+
+        String min = a[0];
+        String max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.compareTo(a[i]) > 0) {
+                min = a[i];
+            }
+            if (max.compareTo(a[i]) < 0) {
+                max = a[i];
+            }
+        }
+        return new Pair<>(min, max);
+    }
+}
