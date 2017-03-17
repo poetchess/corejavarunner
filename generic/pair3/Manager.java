518892f960852e6740e8b7d217ae5628c9e56137
poetchess <ilvbaicai@126.com>
2017-03-17 16:17:53 +0800

    code and comments for section 8.8: wildcard types

---
+++ generic/pair3/Manager.java
@@ -0,0 +1,39 @@
+package pair3;
+
+
+//keyword 'extends' denotes inheritance
+//All inheritance in Java is public inheritance.
+public class Manager extends Employee {
+
+    private double bonus;
+
+    //Every Manager object has four fields:
+    //'name', 'salary' and 'hireDay' are taken from superclass.
+
+    //Call to super() must be the 1st statement in the constructor.
+    //If subclass does not call a superclass constructor explicitly, the
+    //no-argument constructor of the superclass is invoked, in this case,
+    //if the no-argument constructor is missing in the superclass, it will be
+    //a compile time error.
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    //getName() and getHireDay() are automatically inherited from superclass.
+    
+    //override the superclass method
+    //Use keyword 'super' to direct the compiler to call superclass method.
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double b) {
+        bonus = b;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+}
---
+++ generic/pair3/PairTest3.java
@@ -0,0 +1,73 @@
+package pair3;
+
+public class PairTest3 {
+    public static void main(String[] args) {
+        Manager ceo = new Manager("Gus Greedy", 800000, 2003, 12, 15);
+        Manager cfo = new Manager("Sid Sneaky", 600000, 2003, 12, 15);
+        Pair<Manager> buddies = new Pair<>(ceo, cfo);
+        printBuddies(buddies);
+
+        ceo.setBonus(1000000);
+        cfo.setBonus(500000);
+        Manager[] managers = { ceo, cfo };
+
+        Pair<Employee> result = new Pair<>();
+        minmaxBonus(managers, result);
+        System.out.println("first: " + result.getFirst().getName()
+                + ", second: " + result.getSecond().getName());
+        maxminBonus(managers, result);
+        System.out.println("first: " + result.getFirst().getName()
+                + ", second: " + result.getSecond().getName());
+    }
+
+    //The wildcard type Pair<? extends Employee> denotes any generic Pair type 
+    //whose type parameter is a subclass of Employee.
+    //The type Pair<Manager> is a subtype of Pair<? extends Employee>
+    //It leads to safe accessor methods and unsafe mutator methods.
+    public static void printBuddies(Pair<? extends Employee> p) {
+        Employee first = p.getFirst();
+        Employee second = p.getSecond();
+        System.out.println(first.getName() + " and " + second.getName() + 
+                          " are buddies.");
+    }
+
+    //supertype bound: this wildcard is restricted to all supertypes of Manage.
+    //We can supply parameters to methods, but we can’t use the return values.
+    public static void minmaxBonus(Manager[] a, Pair<? super Manager> result) {
+        if (a.length == 0) return;
+        Manager min = a[0];
+        Manager max = a[0];
+        for (int i = 1; i < a.length; i++)
+        {
+            if (min.getBonus() > a[i].getBonus()) min = a[i];
+            if (max.getBonus() < a[i].getBonus()) max = a[i];
+        }
+        result.setFirst(min);
+        result.setSecond(max);
+    }
+
+    public static void maxminBonus(Manager[] a, Pair<? super Manager> result) {
+        minmaxBonus(a, result);
+        PairAlg.swapHelper(result); // OK--swapHelper captures wildcard type
+    }
+}
+
+class PairAlg {
+    //Unbounded Wildcards: The return value of getFirst can only be assigned to
+    //an Object.The setFirst method can never be called, not even with an Object.
+    public static boolean hasNulls(Pair<?> p) {
+        return p.getFirst() == null || p.getSecond() == null;
+    }
+
+    public static void swap(Pair<?> p) { swapHelper(p); }
+
+    //swapHelper is a generic method.
+    //Parameter T of the swapHelper method captures the wildcard.
+    //The compiler must be able to guarantee that the wildcard represents a 
+    //single, definite type.
+    public static <T> void swapHelper(Pair<T> p) {
+        T t = p.getFirst();
+        p.setFirst(p.getSecond());
+        p.setSecond(t);
+    }
+}
